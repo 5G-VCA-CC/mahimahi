56092a256e5c948befd8dc0ef7b7ff89f67a8f60
Nawel Alioua <nawel@ucsb.edu>
2025-01-07 04:48:25 -0800

    Added an abstract class for packet queue classes intended for use with DualPI2 At the moment, this is done mainly to host functions managing the recur count, which is used in deterministic dropping.

---
+++ src/packet/abstract_dualpi2_packet_queue.hh
@@ -0,0 +1,30 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef ABSTRACT_DUALPI2_PACKET_QUEUE_HH
+#define ABSTRACT_DUALPI2_PACKET_QUEUE_HH
+
+#include <random>
+#include <thread>
+#include "dropping_packet_queue.hh"
+
+
+class AbstractDualPI2PacketQueue : public DroppingPacketQueue
+{
+protected:
+    //This constant is copied from link_queue.hh.
+    //It maybe better to get this in a more reliable way in the future.
+    const static unsigned int PACKET_SIZE = 1504; /* default max TUN payload size */
+
+    uint32_t recur_count_;
+
+
+public:
+    AbstractDualPI2PacketQueue( const std::string & args );
+
+    virtual void enqueue( QueuedPacket && p ) = 0;
+
+    uint32_t get_recur_count ( void ) { return recur_count_; } 
+    void set_recur_count ( uint32_t val ) { recur_count_ = val; }
+};
+
+#endif /* ABSTRACT_DUALPI2_PACKET_QUEUE_HH */
