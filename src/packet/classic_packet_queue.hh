c54658df944e050451c627ff7c1d18997f1487ac
Nawel Alioua <nawel@ucsb.edu>
2025-01-07 05:27:08 -0800

    Implemented the L4S native probability function, plus added a few more parameters

--- src/packet/classic_packet_queue.hh
+++ src/packet/classic_packet_queue.hh
@@ -5,21 +5,15 @@
 
 #include <random>
 #include <thread>
-#include "dropping_packet_queue.hh"
+#include "abstract_dualpi2_packet_queue.hh"
 
 /*
    Classic queue implementation as part of the DualPI2 implentation described in RFC 9332.
 */
 
-class CLASSICPacketQueue : public DroppingPacketQueue
+class CLASSICPacketQueue : public AbstractDualPI2PacketQueue
 {
 private:
-    //This constant is copied from link_queue.hh.
-    //It maybe better to get this in a more reliable way in the future.
-    const static unsigned int PACKET_SIZE = 1504; /* default max TUN payload size */
-
-
-
 
     virtual const std::string & type( void ) const override
     {
@@ -37,6 +31,7 @@ public:
     void enqueue( QueuedPacket && p ) override;
 
     QueuedPacket dequeue( void ) override;
+   
 };
 
 #endif /* CLASSIC_PACKET_QUEUE_HH */
--- src/packet/l4s_packet_queue.hh
+++ src/packet/l4s_packet_queue.hh
@@ -5,21 +5,27 @@
 
 #include <random>
 #include <thread>
-#include "dropping_packet_queue.hh"
+#include "abstract_dualpi2_packet_queue.hh"
 
 /*
    L4S queue implementation as part of the DualPI2 implentation described in RFC 9332.
 */
 
-class L4SPacketQueue : public DroppingPacketQueue
+class L4SPacketQueue : public AbstractDualPI2PacketQueue
 {
 private:
-    //This constant is copied from link_queue.hh.
-    //It maybe better to get this in a more reliable way in the future.
-    const static unsigned int PACKET_SIZE = 1504; /* default max TUN payload size */
+    /* L4S native AQM parameters */ 
+    
+    uint64_t max_delay_thresh_us_;
 
+    // Min threshold if using the range method
+    uint64_t min_delay_thresh_us_;
 
+    // Is the marking probability a step or a range function ?
+    bool step_;
 
+    // Minimum queue length for marking to apply
+    uint32_t min_qlen_pkt_;
 
     virtual const std::string & type( void ) const override
     {
@@ -29,7 +35,7 @@ private:
 
     bool drop_early ( void );
 
-    void calculate_drop_prob ( void );
+    
 
 public:
     L4SPacketQueue( const std::string & args );
@@ -37,6 +43,9 @@ public:
     void enqueue( QueuedPacket && p ) override;
 
     QueuedPacket dequeue( void ) override;
+
+    uint32_t calculate_l4s_native_prob ( uint64_t qdelay );
+
 };
 
 #endif /* L4S_PACKET_QUEUE_HH */
