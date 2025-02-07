6974a1b2e1d257198e728fc93a50013703833d13
Nawel Alioua <nawel@ucsb.edu>
2025-02-07 14:35:29 -0800

    Introduce the WRR credit reset if both queues are empty. Reset within the WRR select_queue function.

--- src/packet/weighted_round_robin_scheduler.cc
+++ src/packet/weighted_round_robin_scheduler.cc
@@ -14,42 +14,37 @@ WRRScheduler::WRRScheduler (L4SPacketQueue & l4s_q, CLASSICPacketQueue & classic
 
 QueueType WRRScheduler::select_queue ( ) 
 {
+    if ( l4s_queue_.empty() && classic_queue_.empty() ) {
+        reset_credit();
+        return QueueType::NONE;
+    }
+
     credit_change_ = 0;
-    unsigned int l4s_len_pkt = l4s_queue_.size_packets();
-    unsigned int classic_len_pkt = classic_queue_.size_packets();
 
-    assert ( l4s_len_pkt != 0 || classic_len_pkt != 0 );
-
-    if ( l4s_len_pkt > 0 && (classic_len_pkt == 0 || credit_ <= 0) ) {
+    if ( not l4s_queue_.empty() && (classic_queue_.empty() || credit_ <= 0) ) {
         // The L4S queue can be dequeued.
         // Increase the credit using classic_weight_ if classic queue non-empty
         QueuedPacket* next_pkt = l4s_queue_.peek(); 
-        credit_change_ = classic_len_pkt ? 
+        credit_change_ = not classic_queue_.empty() ? 
             classic_weight_ * next_pkt->contents.size() : 0 ;
         
         return QueueType::L4S;
     }
-    else if (classic_len_pkt > 0) {
+    else if (not classic_queue_.empty()) {
         /* The classic queue can be dequeued.
            Decrease the credit using l4s_weight_ if L4S queue non-empty */ 
         QueuedPacket* next_pkt = classic_queue_.peek(); 
-        credit_change_ = l4s_len_pkt ? 
+        credit_change_ = not l4s_queue_.empty() ? 
             (-1) * l4s_weight_ * next_pkt->contents.size() : 0 ;
         
         return QueueType::Classic;
     }
-    else {
-        // If both queues are empty.
-        // Never happens
-    }
-
-
-
-
+    // else both queues are empty, handled at the beginning.
 }
 
 void WRRScheduler::apply_credit_change ( )
 {
     credit_ += credit_change_ ;
+    credit_change_ = 0;
 }
 
