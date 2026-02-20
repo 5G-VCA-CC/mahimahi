80a963ae0945eadb80464ab07471bbbeb0b7486a
Nawel Alioua <nawel@ucsb.edu>
2026-02-20 15:49:14 -0800

    Remove the minimum queue length condition in the step probability calculation

--- src/packet/l4s_packet_queue.cc
+++ src/packet/l4s_packet_queue.cc
@@ -27,9 +27,9 @@ L4SPacketQueue::L4SPacketQueue( const string & args )
 
 double L4SPacketQueue::calculate_l4s_native_prob ( uint64_t qdelay_ns )
 {
-    if ( size_packets() <= min_qlen_pkt_ )
-        // Do not mark packets if under min_qlen_pkt_ (default is 1)
-        return 0.0;
+    // if ( size_packets() <= min_qlen_pkt_ )
+    //     // Do not mark packets if under min_qlen_pkt_ (default is 1)
+    //     return 0.0;
 
     // In both the step and the ramp methods:
     if ( qdelay_ns > max_delay_thresh_ms_ * NS_PER_MS ) {
